2c396ec57e5f753967a8526967161c4440a13e6f
Phil Siino Hack <phil@neuralgraffiti.com>
2021-11-16 10:39:56 -0800

    Update for publication

--- double-combine.py
+++ double-combine.py
@@ -1,28 +1,41 @@
+"""
+Python script. Given a list of PDF files, combine them into a single document,
+ensuring that each doc will start on a new page if the whole thing is printed
+double-sided.
+
+Requires PyPDF2
+
+:copyright: (c) 2018 by Phil Siino Haack.
+:license: Apache2, see LICENSE for more details.
+"""
+
 import argparse
+import logging
 import sys
+
 
 import PyPDF2
 
-import psh_util
+
 
 def combine():
-    LOGGER = psh_util.get_psh_logger()
+    logger = logging.getLogger("double-combine")
     pdfWriter = PyPDF2.PdfFileWriter()
     # Hack to get around PyPDF2 bug - need to keep files open
     input_files = []
     for in_file in sys.argv[1:]:
-        LOGGER.info("Adding: %s", in_file)
+        logger.info("Adding: %s", in_file)
         in_pdf_file = open(in_file, 'rb')
         input_files.append(in_pdf_file)
         inpdf = PyPDF2.PdfFileReader(in_pdf_file)
-        LOGGER.info("\t %d pages", inpdf.numPages)
+        logger.info("\t %d pages", inpdf.numPages)
         pdfWriter.appendPagesFromReader(inpdf)
         pdfWriter.addBookmark(in_file, pdfWriter.getNumPages()-inpdf.numPages)
         if inpdf.numPages % 2 != 0:
             pdfWriter.addBlankPage()
     out_file = 'combined-%d-files.pdf' % len(input_files)
     with open(out_file, 'wb') as outFileObj:
-        LOGGER.info("Writing %s, %d total pages", 
+        logger.info("Writing %s, %d total pages", 
                     out_file, pdfWriter.getNumPages())
         pdfWriter.write(outFileObj)
     for inf in input_files:
